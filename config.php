a47143657330380ea732b5faf813a8352ed5d646
Jim Boone <jim@unibrain.org>
2022-05-05 09:15:57 -0700

    Added mysql helper class

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Database configuration - sample
+ */
+define('DB_USERNAME', 'practiscore');
+define('DB_PASSWORD', 'practiscore');
+define('DB_HOST',     '127.0.0.1:8889');
+define('DB_NAME',     'practiscore');
